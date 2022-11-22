f7e3c236af61d7677b2f29a4057bba2725aded49
Robert <robertbrignull@github.com>
2022-11-22 14:43:20 +0000

    Add code for generating json schemas

---
+++ src/json-validation.ts
@@ -0,0 +1,18 @@
+import * as fs from "fs";
+
+import Ajv from "ajv";
+
+type Schema = "RepoArray" | "Instructions";
+
+export function validateObject<T>(obj: unknown, schema: Schema): T {
+  const schemaContents = fs.readFileSync(`./json-schemas/${schema}.json`);
+  const ajv = new Ajv();
+  const validate = ajv.compile(schemaContents);
+  const valid = validate(obj);
+  if (!valid) {
+    throw new Error(
+      `Object does not match the "${schema}" schema: ${validate.errors}`
+    );
+  }
+  return obj as T;
+}
