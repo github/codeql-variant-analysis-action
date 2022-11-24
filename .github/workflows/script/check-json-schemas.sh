8d2b9cc971522666289102376ee9b373b5334a2e
Robert <robertbrignull@github.com>
2022-11-24 10:27:31 +0000

    Update some accidentally copy-pasted text

--- .github/workflows/script/check-json-schemas.sh
+++ .github/workflows/script/check-json-schemas.sh
@@ -12,9 +12,9 @@ fi
 # Check that repo is still clean
 if [ ! -z "$(git status --porcelain)" ]; then
     # If we get a fail here then the PR needs attention
-    >&2 echo "Failed: JavaScript files are not up to date. Run 'npm run-script build' to update"
+    >&2 echo "Failed: JSON schema files are not up to date. Run 'script/generate-json-schemas' to update"
     git status
     exit 1
 fi
-echo "Success: JavaScript files are up to date"
+echo "Success: JSON schema files are up to date"
 
