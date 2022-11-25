40b2a5e219061504f40f5ba43239b7086a7dfee1
Robert <robertbrignull@github.com>
2022-11-25 12:51:50 +0000

    Enable @typescript-eslint/prefer-regexp-exec

--- src/deserialize.ts
+++ src/deserialize.ts
@@ -1,7 +1,7 @@
 export function camelize(_: string, value: any): any {
   if (value && typeof value === "object") {
     for (const k in value) {
-      if (k.match(/-./)) {
+      if (/-./.exec(k)) {
         const l = k.replace(/-./g, (x) => x[1].toUpperCase());
         value[l] = value[k];
         delete value[k];
