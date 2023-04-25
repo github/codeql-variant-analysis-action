e4878c1f731b69f2d2412e4b98b1714d76ee2bb9
Robert <robertbrignull@github.com>
2023-04-25 14:40:45 +0100

    Use RetryHelper to upload azure artifacts

--- src/azure-client.ts
+++ src/azure-client.ts
@@ -1,12 +1,36 @@
+import * as core from "@actions/core";
 import FormData from "form-data";
 
-import { getApiClient } from "./download";
+import { getApiClient } from "./api-client";
 import { Policy } from "./gh-api-client";
+import { HTTPError } from "./http-error";
+import { RetryHelper } from "./retry-helper";
 
 export async function uploadArtifact(
   policy: Policy,
   artifactContents: Buffer
 ): Promise<void> {
+  const maxAttempts = 3;
+  const minSeconds = 10;
+  const maxSeconds = 20;
+  const retryHelper = new RetryHelper(maxAttempts, minSeconds, maxSeconds);
+  return await retryHelper.execute(
+    async () => {
+      return await uploadArtifactImpl(policy, artifactContents);
+    },
+    (err: Error) => {
+      if (err instanceof HTTPError && err.httpStatusCode) {
+        // Only retry 504
+        return err.httpStatusCode === 504;
+      }
+
+      // Otherwise abort
+      return false;
+    }
+  );
+}
+
+async function uploadArtifactImpl(policy: Policy, artifactContents: Buffer) {
   const data = new FormData();
   for (const [key, value] of Object.entries(policy.form)) {
     data.append(key, value);
@@ -33,11 +57,9 @@ export async function uploadArtifact(
 
   if (!response.message.statusCode || response.message.statusCode > 299) {
     const responseBody = await response.readBody();
-    console.log(
+    core.warning(
       `Request to ${policy.upload_url} returned status code ${response.message.statusCode}: ${responseBody}`
     );
-    throw new Error(
-      `Failed to upload artifact. Status code: ${response.message.statusCode}).`
-    );
+    throw new HTTPError(response.message.statusCode, responseBody);
   }
 }
