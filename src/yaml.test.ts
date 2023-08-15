457ec8c93e618558d634874986cbd7ae2f69a191
Koen Vlaswinkel <koesie10@github.com>
2023-08-15 10:53:08 +0200

    Switch to yaml for parsing YAML with custom float handling

--- src/yaml.test.ts
+++ src/yaml.test.ts
@@ -2,7 +2,7 @@ import test from "ava";
 
 import { parseYaml } from "./yaml";
 
-test("can successfully parse YAML", (t) => {
+test("can successfully parse YAML with potentially exponential commit SHA", (t) => {
   t.deepEqual(
     {
       sourceLocationPrefix: "/home/runner/work/bulk-builder/bulk-builder",
@@ -10,6 +10,11 @@ test("can successfully parse YAML", (t) => {
       unicodeNewlines: false,
       columnKind: "utf16",
       primaryLanguage: "java",
+      creationMetadata: {
+        sha: "4225332178759948e04347560002921719079454",
+        cliVersion: "2.14.1",
+        creationTime: "2023-08-03T18:19:44.622274245Z",
+      },
       finalised: true,
     },
     parseYaml(`---
@@ -18,6 +23,40 @@ baselineLinesOfCode: 13088
 unicodeNewlines: false
 columnKind: utf16
 primaryLanguage: java
+creationMetadata:
+  sha: 4225332178759948e04347560002921719079454
+  cliVersion: 2.14.1
+  creationTime: 2023-08-03T18:19:44.622274245Z
+finalised: true
+`)
+  );
+});
+
+test("can successfully parse YAML with numeric commit SHA", (t) => {
+  t.deepEqual(
+    {
+      sourceLocationPrefix: "/home/runner/work/bulk-builder/bulk-builder",
+      baselineLinesOfCode: 13088,
+      unicodeNewlines: false,
+      columnKind: "utf16",
+      primaryLanguage: "java",
+      creationMetadata: {
+        sha: "4225332178759948504347560002921719079454",
+        cliVersion: "2.14.1",
+        creationTime: "2023-08-03T18:19:44.622274245Z",
+      },
+      finalised: true,
+    },
+    parseYaml(`---
+sourceLocationPrefix: /home/runner/work/bulk-builder/bulk-builder
+baselineLinesOfCode: 13088
+unicodeNewlines: false
+columnKind: utf16
+primaryLanguage: java
+creationMetadata:
+  sha: 4225332178759948504347560002921719079454
+  cliVersion: 2.14.1
+  creationTime: 2023-08-03T18:19:44.622274245Z
 finalised: true
 `)
   );
--- src/yaml.ts
+++ src/yaml.ts
@@ -1,9 +1,42 @@
 import fs from "fs";
 
-import * as yaml from "js-yaml";
+import { parse, ScalarTag } from "yaml";
+
+// Use a custom tag for floats in exponential notation, to make the +/- mandatory
+// This fixes commit SHAs consisting only of numbers with a single "e" in them
+export const floatExp: ScalarTag = {
+  identify: (value) => typeof value === "number",
+  default: true,
+  tag: "tag:yaml.org,2002:float",
+  format: "EXP",
+  test: /^[-+]?(?:[0-9][0-9_]*)?(?:\.[0-9_]*)?[eE][-+][0-9]+$/, // Change is here, making the [-+] mandatory
+  resolve: (str: string) => parseFloat(str.replace(/_/g, "")),
+  stringify(node) {
+    const num = Number(node.value);
+    if (isFinite(num)) {
+      return num.toExponential();
+    }
+
+    return isNaN(num) ? ".nan" : num < 0 ? "-.inf" : ".inf";
+  },
+};
 
 export function parseYaml<T>(src: string): T {
-  return yaml.load(src) as T;
+  return parse(src, {
+    version: "1.1", // CodeQL CLI uses YAML 1.1
+    schema: "yaml-1.1",
+    customTags: (tags) => {
+      const tagsWithoutFloatExp = tags.filter((tag) => {
+        if (typeof tag !== "object" || !tag.tag) {
+          return true;
+        }
+
+        return tag.tag !== "tag:yaml.org,2002:float" && tag.format !== "EXP";
+      });
+
+      return [floatExp, ...tagsWithoutFloatExp];
+    },
+  }) as T;
 }
 
 export function parseYamlFromFile<T>(filePath: string): T {
